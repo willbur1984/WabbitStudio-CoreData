06aff3a7b545e6e4beac61bb36a842e134f2bed2
William Towe <willbur1984@gmail.com>
2012-10-03 18:53:59 -0400

    turned of non contig layout, turned on background layout

--- WabbitCode/WCLayoutManager.m
+++ WabbitCode/WCLayoutManager.m
@@ -24,8 +24,8 @@
 	
 	[self setTypesetter:[[WCTypesetter alloc] init]];
 	[self setGlyphGenerator:[[WCGlyphGenerator alloc] init]];
-    [self setAllowsNonContiguousLayout:YES];
-    [self setBackgroundLayoutEnabled:NO];
+    [self setAllowsNonContiguousLayout:NO];
+    [self setBackgroundLayoutEnabled:YES];
 	
 	return self;
 }
