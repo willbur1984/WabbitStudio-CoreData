91a54622006e858cfddec89ce6c3cf0dd690d023
William Towe <willbur1984@gmail.com>
2012-10-07 02:00:53 -0400

    changed base sdk to 10.7

---
+++ WabbitEmu/WEDocument.h
@@ -0,0 +1,13 @@
+//
+//  WEDocument.h
+//  WabbitEmu
+//
+//  Created by William Towe on 10/7/12.
+//  Copyright (c) 2012 William Towe. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface WEDocument : NSDocument
+
+@end
---
+++ WabbitEmu/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  WabbitEmu
+//
+//  Created by William Towe on 10/7/12.
+//  Copyright (c) 2012 William Towe. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
