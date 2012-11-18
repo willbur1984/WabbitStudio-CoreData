f8e78ac307a09a282a62777d84deb06755817fb3
William Towe <willbur1984@gmail.com>
2012-11-18 02:25:57 -0500

    added flattenedFiles and flattenedFilesInclusive properties

--- WabbitCode/CoreData/File.h
+++ WabbitCode/CoreData/File.h
@@ -3,6 +3,9 @@
 
 @interface File : _File <WCOpenQuicklyDataSource> {}
 
+@property (readonly,nonatomic) NSArray *flattenedFiles;
+@property (readonly,nonatomic) NSArray *flattenedFilesInclusive;
+
 - (void)sortChildrenUsingComparator:(NSComparator)comparator recursively:(BOOL)recursively;
 
 @end
--- WabbitCode/CoreData/File.m
+++ WabbitCode/CoreData/File.m
@@ -14,6 +14,21 @@
     }
 }
 
+- (NSArray *)flattenedFiles {
+    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:self.entity.name];
+    
+    [fetchRequest setPredicate:[NSPredicate predicateWithFormat:@"self.isGroup == FALSE AND (self.file == %@ OR self.file IN %@)",self,self.files]];
+    
+    return [self.managedObjectContext executeFetchRequest:fetchRequest error:NULL];
+}
+- (NSArray *)flattenedFilesInclusive {
+    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:self.entity.name];
+    
+    [fetchRequest setPredicate:[NSPredicate predicateWithFormat:@"self.isGroup == FALSE AND (self == %@ OR self.file == %@ OR self.file IN %@)",self,self,self.files]];
+    
+    return [self.managedObjectContext executeFetchRequest:fetchRequest error:NULL];
+}
+
 - (NSImage *)image {
     return [[NSWorkspace sharedWorkspace] iconForFile:self.path];
 }
